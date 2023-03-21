c2c9def570b48ac1912cb00edbf7d279c2d5bba7
quangkhai76d1 <18520876@gm.uit.edu.vn>
2023-03-21 18:23:34 +0700

    config prettier, font and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,68 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/common/**/*.{js,ts,jsx,tsx}',
+    './src/layout/**/*.{js,ts,jsx,tsx}',
+    './src/modules/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        '2xl': '1316px',
+        '3xl': '1390px',
+        '4xl': '14866px',
+        '5xl': '1870px',
+      },
+      fontSize: {
+        xs: ['1.2rem', '1.8rem'],
+        sm: ['1.4rem', '2.1rem'],
+        base: ['1.6rem', '2.6rem'],
+        md: ['1.8rem', '2.9rem'],
+        lg: ['2rem', '3.2rem'],
+        xl: ['2.4rem', '3.9rem'],
+        '2xl': ['3.2rem', '3.8rem'],
+        '3xl': ['4.4rem', '6.6rem'],
+        '4xl': ['5.2rem', '7.8rem'],
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        main: '#27dcda',
+        'primary-05': '#070808',
+        'primary-10': '#0d0d0e',
+        'primary-20': '#131315',
+        'primary-30': '#18191b',
+        'primary-50': '#252629',
+        'primary-60': '#515154',
+        'primary-80': '#a8a8a9',
+        'primary-70': '#7c7d7f',
+        'primary-90': '#d3d4d4',
+        'primary-95': '#F4F4F4',
+        'secondary-05': '#6b9898',
+        'secondary-10': '#61a2a1',
+        'secondary-20': '#4eb5b4',
+        'secondary-40': '#3ac9c7',
+        'secondary-40': '#27dcda',
+        'secondary-50': '#1de6e3',
+        'secondary-60': '#14efed',
+        'secondary-80': '#0af9f6',
+        'gray-60': '#b3b3b3',
+        'gray-90': '#f2f2f2',
+        other: '#d3b398',
+      },
+      borderWidth: {
+        DEFAULT: '0.1rem',
+      },
+      transitionDuration: {
+        DEFAULT: '300ms',
+      },
+      transitionTimingFunction: {
+        DEFAULT: 'cubic-bezier(0, 0, 0.2, 1)', //Ease out
+      },
+    },
+  },
+  plugins: [],
+};
